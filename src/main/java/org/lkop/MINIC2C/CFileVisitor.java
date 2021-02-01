ebb4bd3f8f200a92b3312efa577e8d85131ea7eb
Loukas Kopanias <loukaskopanias122@gmail.com>
2021-02-01 03:43:49 +0200

    Creating and printing part of the CFileTree to Graphviz

--- src/main/java/org/lkop/MINIC2C/CFileVisitor.java
+++ src/main/java/org/lkop/MINIC2C/CFileVisitor.java
@@ -7,61 +7,72 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Stack;
 
-public class CFileVisitor extends CodeVisitor{
+public class CFileVisitor extends CodeVisitor<Integer>{
 
     private String filename;
-    private PrintWriter writer;
+    private PrintWriter dot_writer, c_writer;
     private int serial_counter = 0;
     private Stack<Integer> pos = new Stack<>();
 
     public CFileVisitor(String filename) throws FileNotFoundException {
         this.filename = filename;
-        writer = new PrintWriter("output/"+this.filename+".dot");
+        dot_writer = new PrintWriter("output/"+this.filename+".dot");
+        c_writer = new PrintWriter("output/"+this.filename+".c");
     }
 
     private void extractSubgraphs(CodeContainer node, int context, String[] context_names) {
-        writer.println("\tsubgraph cluster" + serial_counter++ + "{");
-        writer.println("\t\tnode [style=filled,color=white];");
-        writer.println("\t\tstyle=filled;");
-        writer.println("\t\tcolor=lightgrey;");
+        dot_writer.println("\tsubgraph cluster" + serial_counter++ + "{");
+        dot_writer.println("\t\tnode [style=filled,color=white];");
+        dot_writer.println("\t\tstyle=filled;");
+        dot_writer.println("\t\tcolor=lightgrey;");
 
-        writer.print("\t\t");
+        dot_writer.print("\t\t");
         for (CodeContainer elem : node.getChildrenInContext(context, pos.peek())) {
             int i = pos.peek();
             pos.pop();
             pos.push(++i);
-            writer.print(elem.getName()+"_"+elem.getSerialId()+"; ");
+            dot_writer.print(elem.getName()+"_"+elem.getSerialId()+"; ");
         }
 
-        writer.println("\n\t\tlabel=" + context_names[context] + ";");
-        writer.println("\t}");
+        dot_writer.println("\n\t\tlabel=" + context_names[context] + ";");
+        dot_writer.println("\t}");
+    }
+
+    private void addToFile(CodeContainer node, int context) {
+        for (CodeContainer elem : node.getChildrenInContext(context, pos.peek())) {
+            int i = pos.peek();
+            pos.pop();
+            pos.push(++i);
+
+            c_writer.print(elem.code);
+        }
     }
 
     @Override
     public Integer visitCodeFile(CodeFile node) {
         System.out.println("CodeVisitableElement -> CodeFile");
 
-        writer.println("digraph G {");
+        dot_writer.println("digraph G {");
 
         pos.push(0);
         extractSubgraphs(node, CodeFile.CC_FILE_PREPROCESSOR, CodeFile.context_names);
-        writer.println("\""+node.getGraphvizName()+"\"->\""+node.getChildrenInContext(CodeFile.CC_FILE_PREPROCESSOR).get(0).getGraphvizName()+"\";");
         pos.pop();
+        dot_writer.println("\""+node.getGraphvizName()+"\"->\""+node.getChildrenInContext(CodeFile.CC_FILE_PREPROCESSOR).get(0).getGraphvizName()+"\";");
 
         pos.push(0);
         extractSubgraphs(node, CodeFile.CC_FILE_GLOBALS, CodeFile.context_names);
-        writer.println("\""+node.getGraphvizName()+"\"->\""+node.getChildrenInContext(CodeFile.CC_FILE_GLOBALS).get(0).getGraphvizName()+"\";");
         pos.pop();
+        dot_writer.println("\""+node.getGraphvizName()+"\"->\""+node.getChildrenInContext(CodeFile.CC_FILE_GLOBALS).get(0).getGraphvizName()+"\";");
 
         pos.push(0);
-        extractSubgraphs(node, CodeFile.CC_FILE_FUNDEF, CodeFile.context_names);
+        extractSubgraphs(node, CodeFile.CC_FILE_FUNCTIONDEFINITION, CodeFile.context_names);
         pos.pop();
-        writer.println("\""+node.getGraphvizName()+"\"->\""+node.getChildrenInContext(CodeFile.CC_FILE_FUNDEF).get(0).getGraphvizName()+"\";");
 
         super.visitCodeFile(node);
 
-        writer.println("}");
-        writer.close();
+        dot_writer.println("}");
+        dot_writer.close();
+        c_writer.close();
 
         try {
             new GIFCreator(filename);
@@ -73,20 +84,110 @@ public class CFileVisitor extends CodeVisitor{
     }
 
     @Override
+    public Integer visitCodeExpressionStatement(CodeExpressionStatement node) {
+        System.out.println("CodeVisitableElement -> CodeExpressionStatement");
+
+        //Graphviz
+        pos.push(0);
+        extractSubgraphs(node, CodeExpressionStatement.CB_EXPRESSION_BODY, CodeExpressionStatement.context_names);
+        pos.pop();
+        dot_writer.println("\""+node.getParent(0).getGraphvizName()+"\"->\""+node.getGraphvizName()+"\";");
+
+        //dot_writer.println("\""+node.getGraphvizName()+"\"->\""+node.getChildrenInContext(CodeExpressionStatement.CB_EXPRESSION_BODY).get(0).getGraphvizName()+"\";");
+
+
+        super.visitCodeExpressionStatement(node);
+        return 0;
+    }
+
+    @Override
+    public Integer visitCodeCompoundStatement(CodeCompoundStatement node) {
+        System.out.println("CodeVisitableElement -> CodeCompoundStatement");
+
+        //Graphviz
+        pos.push(0);
+        extractSubgraphs(node, CodeCompoundStatement.CB_COMPOUND_BODY, CodeCompoundStatement.context_names);
+        pos.pop();
+        dot_writer.println("\""+node.getParent(0).getGraphvizName()+"\"->\""+node.getGraphvizName()+"\";");
+
+        super.visitCodeCompoundStatement(node);
+        return 0;
+    }
+
+    @Override
+    public Integer visitCodeWhileStatement(CodeWhileStatement node) {
+        System.out.println("CodeVisitableElement -> CodeWhileStatement");
+
+        //Graphviz
+        pos.push(0);
+        extractSubgraphs(node, CodeWhileStatement.CB_WHILE_CONDITION, CodeWhileStatement.context_names);
+        pos.pop();
+
+        pos.push(0);
+        extractSubgraphs(node, CodeWhileStatement.CB_WHILE_BODY, CodeWhileStatement.context_names);
+        pos.pop();
+
+        dot_writer.println("\""+node.getParent(0).getGraphvizName()+"\"->\""+node.getGraphvizName()+"\";");
+
+        //Adding to file
+//        c_writer.print("if(");
+//        pos.push(0);
+//        addToFile(node, CodeIfStatement.CB_IF_CONDITION);
+//        pos.pop();
+//
+//        c_writer.println(") {");
+//
+//        pos.push(0);
+//        addToFile(node, CodeIfStatement.CB_IF_BODY);
+//        pos.pop();
+//
+//        c_writer.println("}");
+
+        super.visitCodeWhileStatement(node);
+        return 0;
+    }
+
+    @Override
     public Integer visitCodeIfStatement(CodeIfStatement node) {
         System.out.println("CodeVisitableElement -> CodeIfStatement");
 
+        //Graphviz
         pos.push(0);
-        extractSubgraphs(node, CodeIfStatement.CB_IF_STATEMENT_CONDITION, CodeIfStatement.context_names);
+        extractSubgraphs(node, CodeIfStatement.CB_IF_CONDITION, CodeIfStatement.context_names);
         pos.pop();
 
         pos.push(0);
-        extractSubgraphs(node, CodeIfStatement.CB_IF_STATEMENT_BODY, CodeIfStatement.context_names);
+        extractSubgraphs(node, CodeIfStatement.CB_IF_BODY, CodeIfStatement.context_names);
         pos.pop();
 
-        writer.println("\""+node.getParent(0).getGraphvizName()+"\"->\""+node.getGraphvizName()+"\";");
+        dot_writer.println("\""+node.getParent(0).getGraphvizName()+"\"->\""+node.getGraphvizName()+"\";");
+
+//        //Adding to file
+//        c_writer.print("if(");
+//        pos.push(0);
+//        addToFile(node, CodeIfStatement.CB_IF_CONDITION);
+//        pos.pop();
+//
+//        c_writer.println(") {");
+//
+//        pos.push(0);
+//        addToFile(node, CodeIfStatement.CB_IF_BODY);
+//        pos.pop();
+//
+//        c_writer.println("}");
 
         super.visitCodeIfStatement(node);
+        return 0;
+    }
+
+    @Override
+    public Integer visitCodeRepository(CodeRepository node) {
+        System.out.println("CodeVisitableElement -> CodeRepository");
+
+        //Graphviz
+        dot_writer.println("\""+node.getParent(0).getGraphvizName()+"\"->\""+node.getGraphvizName()+"\";");
+
+        super.visitCodeRepository(node);
         return 0;
     }
 }
--- src/main/java/org/lkop/MINIC2C/MINIC2CTranslationVisitor.java
+++ src/main/java/org/lkop/MINIC2C/MINIC2CTranslationVisitor.java
@@ -2,7 +2,7 @@ package org.lkop.MINIC2C;
 
 import java.util.Stack;
 
-public class MINIC2CTranslationVisitor extends ASTVisitor{
+public class MINIC2CTranslationVisitor extends ASTVisitor<Integer>{
 
     private CodeFile code_file;
     private Stack<CodeContainer> parents = new Stack<>();
@@ -24,7 +24,7 @@ public class MINIC2CTranslationVisitor extends ASTVisitor{
         new_node.addChild(new CodeContainer(CodeNodeType.CB_CODEREPOSITORY, "All_globals", CodeFile.CC_FILE_GLOBALS));
 
         parents.push(new_node);
-        parents_ctx.push(CodeFile.CC_FILE_FUNDEF);
+        parents_ctx.push(CodeFile.CC_FILE_FUNCTIONDEFINITION);
         for (ASTElement elem : node.getChildrenInContext(CCompileUnit.CT_COMPILEUNIT_STATEMENTS)) {
             super.visit(elem);
         }
@@ -35,32 +35,94 @@ public class MINIC2CTranslationVisitor extends ASTVisitor{
     }
 
     @Override
+    public Integer visitCExpressionStatement(CExpressionStatement node) {
+        CodeContainer parent = parents.peek();
+
+        CodeExpressionStatement new_node = new CodeExpressionStatement(parents_ctx.peek());
+        parent.addChild(new_node);
+
+        parents.push(new_node);
+        parents_ctx.push(CodeExpressionStatement.CB_EXPRESSION_BODY);
+        for (ASTElement elem : node.getChildrenInContext(CExpressionStatement.CT_EXRESSION)) {
+            super.visit(elem);
+        }
+        parents_ctx.pop();
+        parents.pop();
+
+        new_node.addCode(";\n");
+
+        parent.addCode(new_node);
+        return 0;
+    }
+
+    @Override
+    public Integer visitCCompound(CCompound node) {
+        CodeContainer parent = parents.peek();
+
+        CodeCompoundStatement new_node = new CodeCompoundStatement(parents_ctx.peek());
+        parent.addChild(new_node);
+
+        parents.push(new_node);
+        parents_ctx.push(CodeCompoundStatement.CB_COMPOUND_BODY);
+        for (ASTElement elem : node.getChildrenInContext(CCompound.CT_COMPOUND_STATEMENTSLIST)) {
+            super.visit(elem);
+        }
+        parents_ctx.pop();
+        parents.pop();
+
+        parent.addCode(new_node);
+        return 0;
+    }
+
+    @Override
     public Integer visitCIf(CIf node) {
         CodeContainer parent = parents.peek();
 
         CodeIfStatement new_node = new CodeIfStatement(parents_ctx.peek());
         parent.addChild(new_node);
 
-        new_node.addCode("if (");
         parents.push(new_node);
-        parents_ctx.push(CodeIfStatement.CB_IF_STATEMENT_CONDITION);
+        parents_ctx.push(CodeIfStatement.CB_IF_CONDITION);
         for (ASTElement elem : node.getChildrenInContext(CIf.CT_IF_EXPRESSION)) {
             super.visit(elem);
         }
         parents_ctx.pop();
         parents.pop();
 
-        new_node.addCode(") {\n");
-
         parents.push(new_node);
-        parents_ctx.push(CodeIfStatement.CB_IF_STATEMENT_BODY);
+        parents_ctx.push(CodeIfStatement.CB_IF_BODY);
         for (ASTElement elem : node.getChildrenInContext(CIf.CT_IF_STATEMENT)) {
             super.visit(elem);
         }
         parents_ctx.pop();
         parents.pop();
 
-        new_node.addCode("}\n");
+        parent.addCode(new_node);
+        return 0;
+    }
+
+    @Override
+    public Integer visitCWhile(CWhile node) {
+        CodeContainer parent = parents.peek();
+
+        CodeWhileStatement new_node = new CodeWhileStatement(parents_ctx.peek());
+        parent.addChild(new_node);
+
+        parents.push(new_node);
+        parents_ctx.push(CodeWhileStatement.CB_WHILE_CONDITION);
+        for (ASTElement elem : node.getChildrenInContext(CWhile.CT_WHILE_EXPRESSION)) {
+            super.visit(elem);
+        }
+        parents_ctx.pop();
+        parents.pop();
+
+        parents.push(new_node);
+        parents_ctx.push(CodeWhileStatement.CB_WHILE_BODY);
+        for (ASTElement elem : node.getChildrenInContext(CWhile.CT_WHILE_STATEMENT)) {
+            super.visit(elem);
+        }
+        parents_ctx.pop();
+        parents.pop();
 
         parent.addCode(new_node);
         return 0;
@@ -68,7 +130,7 @@ public class MINIC2CTranslationVisitor extends ASTVisitor{
 
     @Override
     public Integer visitCAssignment(CAssignment node) {
-        CodeAssignment new_node = new CodeAssignment(parents_ctx.peek());
+        CodeRepository new_node = new CodeRepository(parents_ctx.peek());
 
         CodeContainer parent = parents.peek();
         parent.addChild(new_node);
@@ -90,6 +152,55 @@ public class MINIC2CTranslationVisitor extends ASTVisitor{
         parents.pop();
 
         parent.addCode(new_node);
+        return 0;
+    }
+
+    @Override
+    public Integer visitCGt(CGt node) {
+        return super.visitCGt(node);
+    }
+
+    @Override
+    public Integer visitCGte(CGte node) {
+        return super.visitCGte(node);
+    }
+
+    @Override
+    public Integer visitCLt(CLt node) {
+        return super.visitCLt(node);
+    }
+
+    @Override
+    public Integer visitCLte(CLte node) {
+        return super.visitCLte(node);
+    }
+
+    @Override
+    public Integer visitCEqual(CEqual node) {
+        return super.visitCEqual(node);
+    }
+
+    @Override
+    public Integer visitCNequal(CNequal node) {
+        CodeRepository new_node = new CodeRepository(parents_ctx.peek());
+
+        CodeContainer parent = parents.peek();
+        parent.addChild(new_node);
+
+        parents.push(new_node);
+        for (ASTElement elem : node.getChildrenInContext(CNequal.CT_LEFT)) {
+            super.visit(elem);
+        }
+        parents.pop();
+
+        new_node.addCode("!=");
+
+        parents.push(new_node);
+        for (ASTElement elem : node.getChildrenInContext(CNequal.CT_RIGHT)) {
+            super.visit(elem);
+        }
+        parents.pop();
+
         return 0;
     }
 
